d4804803b9051e87eac37cf0848c553d1361cfc8
Richard <richard@MacBook-Air-de-Richard.local>
2025-04-22 14:38:44 -0300

    liks para pagnias

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -24,18 +24,16 @@ export default function NavBar() {
 
       {/* Menu Desktop */}
       <ul className="hidden sm:flex gap-6 text-white font-medium">
-        <li><Link href="#quem-somos" className="hover:text-orange-500">Quem Somos</Link></li>
-        <li><Link href="#servicos" className="hover:text-orange-500">Serviços</Link></li>
-        <li><Link href="#diferenciais" className="hover:text-orange-500">Diferenciais</Link></li>
-        <li><Link href="#trabalhe" className="hover:text-orange-500">Trabalhe Conosco</Link></li>
-        <li><Link href="#contato" className="hover:text-orange-500">Contato</Link></li>
+        <li><Link href="quem-somos" className="hover:text-orange-500">Quem Somos</Link></li>
+        <li><Link href="servicos" className="hover:text-orange-500">Serviços</Link></li>
+        <li><Link href="trabalhe-conosco" className="hover:text-orange-500">Trabalhe Conosco</Link></li>
+        <li><Link href="contato" className="hover:text-orange-500">Contato</Link></li>
       </ul>
 
       {/* Menu Mobile Dropdown */}
       {isOpen && (
         <ul className="absolute top-full left-0 w-full bg-[#1c1c1c] text-white text-sm flex flex-col gap-4 py-4 px-6 sm:hidden z-50 border-t border-neutral-700">
           <li><Link href="quem-somos" onClick={() => setIsOpen(false)} className="hover:text-orange-500">Quem-Somos</Link></li>
-          <li><Link href="#servicos" onClick={() => setIsOpen(false)} className="hover:text-orange-500">Serviços</Link></li>
           <li><Link href="#diferenciais" onClick={() => setIsOpen(false)} className="hover:text-orange-500">Diferenciais</Link></li>
           <li><Link href="#trabalhe" onClick={() => setIsOpen(false)} className="hover:text-orange-500">Trabalhe Conosco</Link></li>
           <li><Link href="#contato" onClick={() => setIsOpen(false)} className="hover:text-orange-500">Contato</Link></li>
